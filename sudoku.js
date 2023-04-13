be830a0639b3d3e919c6ea8b3ebcdcdbb0a7bb1d
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-04-13 21:05:35 +0200

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,10 @@
-const numSelected = null;
-const tileSelected = null;
+let numSelected = null;
+let tileSelected = null;
 
 let errors = 0;
 
 let board = [
-  "--7491-5",
+  "--74916-5",
   "2---6-3-9",
   "-----7-1-",
   "-586----4",
@@ -32,23 +32,63 @@ window.onload = function () {
 };
 
 function setGame() {
-  //digits
+  // Digits 
   for (let i = 1; i <= 9; i++) {
     //<div id="1" class="number">1</div>
     let number = document.createElement("div");
     number.id = i;
     number.innerText = i;
+    number.addEventListener("click", selectNumber);
     number.classList.add("number");
     document.getElementById("digits").appendChild(number);
   }
 
-  //board
+  // Board 
   for (let r = 0; r < 9; r++) {
     for (let c = 0; c < 9; c++) {
       let tile = document.createElement("div");
       tile.id = r.toString() + "-" + c.toString();
+      if (board[r][c] != "-") {
+        tile.innerText = board[r][c];
+        tile.classList.add("tile-start");
+      }
+      if (r == 2 || r == 5) {
+        tile.classList.add("horizontal-line");
+      }
+      if (c == 2 || c == 5) {
+        tile.classList.add("vertical-line");
+      }
+      tile.addEventListener("click", selectTile);
       tile.classList.add("tile");
       document.getElementById("board").append(tile);
+    }
+  }
+}
+
+function selectNumber() {
+  if (numSelected != null) {
+    numSelected.classList.remove("number-selected");
+  }
+  numSelected = this;
+  numSelected.classList.add("number-selected");
+}
+
+function selectTile() {
+  if (numSelected) {
+    if (this.innerText != "") {
+      return;
+    }
+
+    
+    let coords = this.id.split("-"); //["0", "0"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    if (solution[r][c] == numSelected.id) {
+      this.innerText = numSelected.id;
+    } else {
+      errors += 1;
+      document.getElementById("errors").innerText = errors;
     }
   }
 }
